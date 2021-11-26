c3e50fe6b16d80ba489ff32e312a96a6b2405da8
Bidisha-Roy <78555584+Bidisha-Roy@users.noreply.github.com>
2021-11-26 12:39:06 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include_once("./assign.html");?>
+<?php include_once("./submit.html");?>
+<?php
+   //include CSS Style Sheet
+   echo "<link rel='stylesheet' type='text/css' href='./css/style.css' />";
+
+   
+?>
+
