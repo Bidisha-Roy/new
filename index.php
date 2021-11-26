850efb6a7c962e59641112bbc0b121bf0131f0b5
Bidisha-Roy <78555584+Bidisha-Roy@users.noreply.github.com>
2021-11-26 13:15:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php include_once("./submit.html");?>
 <?php
    //include CSS Style Sheet
-   echo "<link rel='stylesheet' type='text/css' href='./css/style.css' />";
+   echo "<link rel='stylesheet' type='text/css' href='./style.css' />";
 
    
 ?>
